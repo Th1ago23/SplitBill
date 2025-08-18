595ce96eab06bb99069c6ec489a44f9643fde2a3
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-08-18 15:30:40 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Entity/Grupo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Group
+    {
+        [Key]
+        public int Id { get; private set; }
+        [Required]
+        public string Name { get; set; }
+        public ICollection<User> Users { get; set; }
+    
+    }
+}
---
+++ Domain/Entity/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public int Id { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+        [Required]
+        public string Username { get; set; }
+        
+        public string FullName { get; set; }
+        
+        public DateTime BirthdayDate { get; set; }
+
+    }
+}
